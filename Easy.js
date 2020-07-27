7dc922e69de56f9d4fbd03b1af1f7479c730b09e
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2020-07-27 13:04:44 +0200

    Create Easy.js

---
+++ Easy.js
@@ -0,0 +1,25 @@
+'use strict'
+onmessage = messageEvent => {
+	let length = (messageEvent.data.gameboard.length/2) - 2; // -2?
+	let lengthFull = messageEvent.data.gameboard.length - 1;	// Do not count opposite store.
+	let index = -1;
+	// Go from right.
+	for(let i = length; 0 <= i; i--){
+		let stepsToStore = length - i + 1;
+		// Look for first match to store.
+		if(messageEvent.data.gameboard[i]%lengthFull === stepsToStore){
+			index = i;
+			postMessage(index);
+			break;
+		}
+	}
+	if(index === -1){
+		// Select first from left with pallets.
+		for(let i = length; 0 <= i; i--){
+			if(0 < messageEvent.data.gameboard[i]){
+				postMessage(i);
+				break;
+			}
+		}
+	}
+};
