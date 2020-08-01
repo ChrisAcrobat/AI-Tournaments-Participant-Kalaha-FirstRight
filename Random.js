491783eab4a97eee33fc226759926d04f67c4216
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2020-08-01 16:13:59 +0200

    Update Random.js

--- Random.js
+++ Random.js
@@ -2,8 +2,7 @@
 onmessage = messageEvent => {
 	let length = (messageEvent.data.gameboard.length/2) - 2;
 	let index;
-	do
-	{
+	do{
 		index = Math.round(Math.random()*length);
 	}while(0 == messageEvent.data.gameboard[index])); // Readded ")" to force a syntax error. Testing arena error handling.
 	postMessage(index);
