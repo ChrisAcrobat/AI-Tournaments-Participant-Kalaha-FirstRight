de6d157c0436c6232f98d42243c3a6f8bd7f986d
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2020-08-09 10:25:55 +0200

    Create _Test_Error_Select0.js

---
+++ _Test_Error_Select0.js
@@ -0,0 +1,16 @@
+'use strict'
+onmessage = messageEvent => {
+	let length = (messageEvent.data.gameboard.length/2) - 1;
+	for(let i = 0; 0 < length; i++){
+		if(0 === messageEvent.data.gameboard[i]){
+			postMessage(i);
+			return;
+		}
+	}
+	for(let i = 0; 0 < length; i++){
+		if(0 < messageEvent.data.gameboard[i]){
+			postMessage(i);
+			break;
+		}
+	}
+};
