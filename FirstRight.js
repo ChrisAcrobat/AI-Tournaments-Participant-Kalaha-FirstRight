027dee999a728da8e9d030cd64f5e1a5a8d15348
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2020-08-16 21:41:49 +0200

    Update FirstRight.js

--- FirstRight.js
+++ FirstRight.js
@@ -1,10 +1,13 @@
 'use strict'
-onmessage = messageEvent => {
-	let length = (messageEvent.data.gameboard.length/2) - 2;
+function play(messageEvent){
+	let length = (messageEvent.data.length/2) - 2;
 	for(let i = length; 0 <= i; i--){
-		if(0 < messageEvent.data.gameboard[i]){
+		if(0 < messageEvent.data[i]){
 			postMessage(i);
 			return;
 		}
 	}
+}
+onmessage = messageEvent => {
+	onmessage = play;
 };
