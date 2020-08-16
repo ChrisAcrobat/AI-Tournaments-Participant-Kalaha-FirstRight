3a711b73f2723c417a6d7859bfdfc4dc1467ec8e
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2020-08-16 21:44:53 +0200

    Update _Test_Error_Select0.js

--- _Test_Error_Select0.js
+++ _Test_Error_Select0.js
@@ -1,5 +1,5 @@
 'use strict'
-onmessage = messageEvent => {
+function play(messageEvent){
 	let length = (messageEvent.data.gameboard.length/2) - 1;
 	for(let i = 0; 0 < length; i++){
 		if(0 === messageEvent.data.gameboard[i]){
@@ -13,4 +13,7 @@ onmessage = messageEvent => {
 			break;
 		}
 	}
+}
+onmessage = messageEvent => {
+	onmessage = play;
 };
