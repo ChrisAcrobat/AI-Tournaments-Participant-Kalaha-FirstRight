d68b3775ceb3480e0d529ca7a1f5f9d2ccb7ce5c
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2022-04-10 10:38:06 +0200

    Update participant.js

--- participant.js
+++ participant.js
@@ -1,11 +1,13 @@
 'use strict'
-ParticipantHelper.onmessage = message => {
-	let data = message.data;
-	let length = (data.length/2) - 2;
-	for(let i = length; 0 <= i; i--){
-		if(0 < data[i]){
-			message.respond(i);
-			return;
+onmessage = init => {
+	onmessage = message => {
+		let data = message.data;
+		let length = (data.length/2) - 2;
+		for(let i = length; 0 <= i; i--){
+			if(0 < data[i]){
+				postMessage(i);
+				return;
+			}
 		}
 	}
 }
